5031dc8a97d710972700c6b6f57ae2e29add0e91
Srboljub Stepanovic <srboljub.stepanovic@abo.fi>
2020-02-14 22:22:26 +0200

    Updates

---
+++ precache-manifest.c1a5491c9c5bcd9dcfa180a9aafb0fc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d68dc78069050d5d7781",
+    "url": "/SPMS_SL/static/css/main.b182e374.chunk.css"
+  },
+  {
+    "revision": "d68dc78069050d5d7781",
+    "url": "/SPMS_SL/static/js/main.d68dc780.chunk.js"
+  },
+  {
+    "revision": "76e48ab6856d120cb421",
+    "url": "/SPMS_SL/static/js/1.76e48ab6.chunk.js"
+  },
+  {
+    "revision": "ccf48ca276a93e99d4c4",
+    "url": "/SPMS_SL/static/js/runtime~main.ccf48ca2.js"
+  },
+  {
+    "revision": "598650dc699522486abd27aa997901e3",
+    "url": "/SPMS_SL/index.html"
+  }
+];
